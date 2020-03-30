8aaa9dc932c064bad961f7a93f40e09f1a21a755
gpi <gpimpale29@gmail.com>
2020-03-30 04:38:12 -0700

    further changes

--- public/js/check.js
+++ public/js/check.js
@@ -1,14 +1,14 @@
-/* global L */
+/* global L fetchJSON */
 
-$(function() {
+$(function () {
   $('input[name="daterange"]').daterangepicker({
     opens: 'left'
-  }, function(start, end, label) {
+  }, function (start, end, label) {
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
 
-async function loadmap() {
+function loadmap() {
   let leafletmap = L.map('mapid');
   leafletmap.setView([0, 0], 2);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -23,14 +23,45 @@ async function loadmap() {
 }
 
 
-$(function() {
+$(function () {
   $('input[name="daterange"]').daterangepicker({
     opens: 'left'
-  }, function(start, end, label) {
+  }, function (start, end, label) {
+    // TODO
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
 
+const SCALAR_E7 = 0.0000001;
+
+async function process(file) {
+  let chunkrequestlist = [];
+  let pts = JSON.parse(await file.text()).locations.map(loc => {
+    const l =  {
+      lat: loc.latitudeE7 * SCALAR_E7,
+      lng: loc.longitudeE7 * SCALAR_E7,
+      tmprl: loc.timestampMS / (1000 * 60 * 60 * (100))
+    };
+    // Each chunk is a square 10th of a degree
+    chunkrequestlist.push({
+      lat: Math.round(l.lat*10)/10,
+      lng: Math.round(l.lng*10)/10,
+      tmprl: Math.round(l.tmprl*10)/10,
+    });
+    return l;
+  });
+
+  let chunks = chunkrequestlist.map(chr => await fetchJSON(chr));
+}
+
+
+function loadfilehandler() {
+  $('#customFile').change(async function () {
+    await process(this.files[0]);
+  });
+}
+
 $(document).ready(async function () {
-  await loadmap();
+  loadmap();
+  loadfilehandler();
 });
