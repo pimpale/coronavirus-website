c006f6547afeac26e5a67f1ad25a5df8be47cb7a
gpi <gpimpale29@gmail.com>
2020-03-30 00:29:12 -0700

    added the more libraries

---
+++ public/js/check.js
@@ -0,0 +1,21 @@
+/* global L */
+
+async function loadmap() {
+  let leafletmap = L.map('mapid');
+  leafletmap.setView([0, 0], 2);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 2,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGltcGFsZSIsImEiOiJjazhkbzk4NTIwdHkzM21vMWFiNHIzZ3BiIn0.nLv4P71SFh4TIANuwJ8I9A'
+  }).addTo(leafletmap);
+}
+
+
+
+
+$(document).ready(async function () {
+});
