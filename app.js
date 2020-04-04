136b92ad1bd90bfa2bb68c428b6b2a1cc9092a7b
gpi <gpimpale29@gmail.com>
2020-04-04 14:54:55 -0700

    nice

--- app.js
+++ app.js
@@ -1,99 +1,91 @@
-'use strict'
+'use strict';
 // Imports
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
-const compression = require('compression')
+const compression = require('compression');
 
 let client;
 let db;
 let app;
 
 
+/**
+ * Takes a post request, and loads the body into the mongodb
+ * Dont call manually
+ * @param {req} req The express request
+ * @param {res} res The express response
+ */
 function uploadLocations(req, res) {
-
-  db.insertMany(req.body.map(x => ({ 
-    latitude: x.latitude, 
-    longitude: x.longitude, 
-    timestamp: x.timestamp, 
-    ip: req.ip
+  db.insertMany(req.body.map((x) => ({
+    latitude: x.latitude,
+    longitude: x.longitude,
+    timestamp: x.timestamp,
+    ip: req.ip,
   })), (err, _) => {
-    if(err) {
+    if (err) {
       throw err;
     }
-  })
-
-
-
-  let chunk = {
-    chunk_lat: Math.round(latitude * 10),
-    chunk_lng: Math.round(longitude * 10),
-    chunk_tmprl: Math.round(temporal * 10),
-  }
-
-  let loc = {
-    lat: latitude,
-    lng: longitude,
-    tmprl: temporal
-  };
-
-  if (chunk in mapdata) {
-    mapdata[chunk].push(loc);
-  } else {
-    mapdata[chunk] = [loc];
-  }
-
-  res.end()
+  });
+  res.end();
 }
 
+/**
+ * Takes a get request and gives all the files in the chunk
+ * Dont call manually
+ * @param {req} req The express request
+ * @param {res} res The express response
+ */
 function downloadChunk(req, res) {
-  let chunk = {
+  const chunk = {
     chunk_lat: req.query.chunk_lat,
     chunk_lng: req.query.chunk_lng,
     chunk_tmprl: req.query.chunk_tmprl,
-  }
+  };
   console.log('got here!:', chunk);
 
   res.send(mapdata[chunk]);
-  res.end()
+  res.end();
 }
 
+/**
+ * Initializes the mongodb database, creating collections if they don't
+ * already exist also initializes express
+ */
 async function initialize() {
   // Initialize mongodb connection
-  client = await mongodb.MongoClient.connect(`mongodb://localhost:27017`, {useNewUrlParser: true, useUnifiedTopology: true});
+  client = await mongodb.MongoClient.connect(`mongodb://localhost`, {useNewUrlParser: true, useUnifiedTopology: true});
   db = client.db('test');
 
   // If we haven't initialized a collection, do so now
-  if (!db.listCollections({name: 'locations'}).hasNext()) {
-    db.createCollection("locations",
-      {
-        validator: {
-          $jsonSchema: {
-            bsonType: "object",
-            required: ["latlng", "longitude", "timestamp", "ip"],
-            properties: {
-              latitude: {
-                bsonType: "number",
-                description: "the latitude of this point"
-              },
-              longitude: {
-                bsonType: "number",
-                description: "the longitude of this point"
-              },
-              timestamp: {
-                bsonType: "integer",
-                description: "the milliseconds since 1970 of this point"
-              },
-              ip: {
-                bsonType: "string",
-                description: "ip address of the submitter"
-              }
-            }
-          }
-        }
-      });
-    db.locations.createIndex({latitude:1, longitude:1, timestamp:1});
+  if (!await db.listCollections({name: 'locations'}).hasNext()) {
+    await db.createCollection('locations', {
+      validator: {
+        $jsonSchema: {
+          bsonType: 'object',
+          required: ['latlng', 'longitude', 'timestamp', 'ip'],
+          properties: {
+            latitude: {
+              bsonType: 'double',
+              description: 'the latitude of this point',
+            },
+            longitude: {
+              bsonType: 'double',
+              description: 'the longitude of this point',
+            },
+            timestamp: {
+              bsonType: 'long',
+              description: 'the milliseconds since 1970 of this point',
+            },
+            ip: {
+              bsonType: 'string',
+              description: 'ip address of the submitter',
+            },
+          },
+        },
+      },
+    });
+    await db.locations.createIndex({latitude: 1, longitude: 1, timestamp: 1});
   }
 
   app = express();
@@ -106,9 +98,12 @@ async function initialize() {
   app.get('/api/downloadchunk/', downloadChunk);
 
   // serve static files
-  app.use(express.static('public'));
+  app.use(express.static('webapp'));
 }
 
+/**
+ * Executes the tasks of the app
+ */
 async function main() {
   await initialize();
   app.listen(8080, () => console.log(`App started successfully!`));
