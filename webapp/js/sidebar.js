f593567fcd251e97a414be5d7068aa82e0b7e6d1
gpi <gpimpale29@gmail.com>
2020-04-21 13:56:25 -0700

    removed

--- webapp/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-$(document).ready(function() {
-  $('#sidebar-toggle').click(function() {
-    $(this).blur();
-    $('#sidebar').toggle();
-    $('#content-wrapper').toggleClass('not-sidebar');
-  });
-});
